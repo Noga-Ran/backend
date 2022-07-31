f66e92f90a577b00a6c9073600448bce3a2b6114
Noga <nogaran01@gmail.com>
2022-07-31 18:15:18 +0300

    fronend

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(session)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('public'))
+  app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
   const corsOptions = {
     origin: ['http://127.0.0.1:5174','http://localhost:5174','http://127.0.0.1:5173','http://localhost:5173','http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:8081', 'http://localhost:8081', 'http://127.0.0.1:3000', 'http://localhost:3000'],
